9d5a8ee9c833f7b4285677cbb3f649192219e67c
Kapil Thapa <kapildthapa@hotmail.com>
2022-06-20 00:49:11 -0400

    Completion of mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,16 @@
 const http = require('http');
-const mongoose = require('mongoose') ;
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const hostname = 'localhost';
 const port = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://kapildthapa:H3ll0M0ng0@nasacluster.adq7d.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    await mongoConnect();
     await loadPlanetsData();
     
     server.listen(port, hostname, () => {
